403d05e74fb4288a7b65f2686bc419bbd006f699
dw-002 <dw-002@DESKTOP-G635MMJ>
2026-01-02 16:38:42 +0900

    a

---
+++ java_control/src/study20260102/MainMethod.java
@@ -0,0 +1,37 @@
+package study20260102;
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+ /* 제어문
+ 	
+ 	조건문 - if , switch
+ 	
+ 	1.
+ 	if ( 조건식 ) {
+ 		조건식이 참일경우 실행할 코드 ;
+ 	} 
+ 	
+ 	2.
+ 	if ( 조건식 ) {
+ 		조건식이 참일 경우 실행할 코드;
+ 	}else {
+ 		조건식이 거짓일 경우 실행 할 코드;
+ 	}
+ 	
+ 	3. 
+ 	if ( 조건식1 ) {
+ 		조건식1이 참인 경우에 실행
+ 	} else{
+ 		조건식1이 거짓인 경우에 실행
+ 		if( 조건식2 ) {
+ 		조건식2이 참인 경우에 실행
+ 		}
+ 	}
+ 	
+ */
