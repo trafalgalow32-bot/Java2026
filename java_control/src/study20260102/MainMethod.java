dda25a41c32f3e595b412dc314374832680ab184
dw-002 <dw-002@DESKTOP-G635MMJ>
2026-01-05 09:19:59 +0900

    modified again

--- java_control/src/study20260102/MainMethod.java
+++ java_control/src/study20260102/MainMethod.java
@@ -4,9 +4,34 @@ public class MainMethod {
 
 	public static void main(String[] args) {
 		
+		if (10 < 5) {
+			System.out.println( " 5보다 크다 ");
+		} else {
+			System.out.println( " 5보다 작다");
+		}
+		// 바이킹 탑승 가능 : 145cm 이상
+		
+		int tall = 160;
+		
+		if (tall >= 145 ) {
+			System.out.println("바이킹 탑승가능");
+		} else {
+			System.out.println("바이킹 탑승 불가");			
+		}
+		
+		int age = 33;
+		
+		if ((age/10) == 3) // age >= 30 && age < 40) 
+			System.out.println( " 30대 이다");
+		else
+			System.out.println( " 20대 이다");
+		
+		
 	}
+	
+		
 
-}
+}	
 
  /* 제어문
  	
@@ -34,4 +59,20 @@ public class MainMethod {
  		}
  	}
  	
- */
+ 	4. (2번째, 4번째가 많이 사용됨!!)
+ 	if ( 조건식1 ) {
+ 		조건식1이 참일 경우 실행
+ 	} else if ( 조건식2 ) {
+ 		조건식2가 참일 경우 실행
+ 	}
+ 	
+ 	
+ 	 */ 	
+ 
+	//과제 : 주차장에 요금이 기본요금은 1000원이다. 
+	// 기본 시간은 30분이다.
+	// 10분 당 100원씩 추가 (예: 39분은 1000원, 40분은 1100원 )
+	// 단, 4시간 이상 주차시 기본 요금은 2500원이다. 
+	// (예: 3시간 50분 (230분) - 3000원, 4시간 12분 (252분) -> 2600원) 
+	// 이걸 if문으로 구현! 생성형 AI 절대 사용 금지, 구글링까지만 허용!!
+	// 코드 입력 전, 직접 종이에 적어서 정리하며 생각도 해보기!
